dd6465f557b966ff0238bd817a81ae0834b11fa1
DanishDevX <f24mmg60@aror.edu.pk>
2025-01-19 18:35:43 +0500

    Add files via upload

---
+++ task01.java
@@ -0,0 +1,11 @@
+public class Outputs {
+    public static void main(String[] args) {
+        // Print the data on the console as shown in the example
+        System.out.println("Student Points");
+        System.out.println();
+        System.out.println("Lab\t\t43");
+        System.out.println("39\t\tBomca");
+        System.out.println("Total\t\t10");
+        System.out.println("58\t\t49");
+    }
+}
